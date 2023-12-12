84405f9ec5cc9f636b565be41be443da70fc472a
KonstantinVrunouski <enotsith@gmail.com>
2023-12-12 22:12:45 +0300

    Исправил аргументы функций. Добавил условия.

--- laboratory-task-2/main2.cpp
+++ laboratory-task-2/main2.cpp
@@ -1,5 +1,6 @@
 /*
-Нет доступа к условию.
+14. числа, у которых сумма цифр равна квадрату максимальной
+цифры; (22, 112, 333, 233)
 */
 
 #include <iostream>
@@ -14,13 +15,13 @@ void getBorders(int32_t& num_left_border, int32_t& num_right_border)
 	}
 }
 
-void findNumSumDigitsMoreSquare(int32_t num_left_border, int32_t num_right_border, int32_t temp, int32_t max, int32_t digits_sum, bool is_out)
+void findNumSumDigitsMoreSquare(int32_t num_left_border, int32_t num_right_border)
 {
+	int32_t temp, max, digits_sum;
+	bool is_out;
 	for (size_t i = num_left_border; i <= num_right_border; i++)
 	{
-		temp = i;
-		max = 0;
-		digits_sum = 0;
+		temp = i, max = 0, digits_sum = 0;
 		while (temp != 0)
 		{
 			if (max < temp % 10)
@@ -36,7 +37,7 @@ void findNumSumDigitsMoreSquare(int32_t num_left_border, int32_t num_right_borde
 			is_out = true;
 		}
 	}
-	if (is_out == false)
+	if (!is_out)
 	{
 		std::cout << "No such numbers";
 	}
@@ -44,13 +45,20 @@ void findNumSumDigitsMoreSquare(int32_t num_left_border, int32_t num_right_borde
 
 int main()
 {
-	int32_t num_left_border, num_right_border, digits_sum = 0, temp, max;
-	bool is_out = false;
-	getBorders(num_left_border, num_right_border);
-	if (num_right_border < num_left_border)
+	try
 	{
-		std::swap(num_left_border, num_right_border);
+		int32_t num_left_border, num_right_border, digits_sum = 0, temp, max;
+		bool is_out = false;
+		getBorders(num_left_border, num_right_border);
+		if (num_right_border < num_left_border)
+		{
+			std::swap(num_left_border, num_right_border);
+		}
+		findNumSumDigitsMoreSquare(num_left_border, num_right_border);
 	}
-	findNumSumDigitsMoreSquare(num_left_border, num_right_border,temp, max, digits_sum, is_out);
+	catch (std::exception e)
+	{
+		std::cout << e.what();
+	}
 	return 0;
 }
