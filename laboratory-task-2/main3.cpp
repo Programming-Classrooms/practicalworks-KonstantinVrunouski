d071bf2c8b12e9c6c46dd54effbed2c29892a369
KonstantinVrunouski <enotsith@gmail.com>
2023-12-24 19:23:50 +0300

    Исправил скобки.

--- laboratory-task-2/main3.cpp
+++ laboratory-task-2/main3.cpp
@@ -13,8 +13,7 @@ int32_t inputNumber(const char* message = "Enter natural number:")
     int32_t num;
     std::cout << message << '\n';
     std::cin >> num;
-    if (num <= 0)
-    {
+    if (num <= 0){
         throw "Invalid number.";
     }
     return num;
@@ -23,8 +22,7 @@ int32_t inputNumber(const char* message = "Enter natural number:")
 int32_t factorial(int32_t factor) 
 {
     int32_t result = 1;
-    for (size_t i = 1; i <= factor; i++) 
-    {
+    for (size_t i = 1; i <= factor; i++){
         result *= i;
     }
     return result;
@@ -32,21 +30,16 @@ int32_t factorial(int32_t factor)
 
 void fractPrint(int32_t num, int32_t denominator)
 {
-    for (size_t j = denominator; j > 2; --j)
-    {
+    for (size_t j = denominator; j > 2; --j){
         int32_t numerator = denominator - j;
         int32_t tempDenominator = denominator;
-
-        for (size_t i = denominator; i >= 1; --i)
-        {
-            if (tempDenominator % i == 0 && numerator % i == 0)
-            {
+        for (size_t i = denominator; i >= 1; --i){
+            if (tempDenominator % i == 0 && numerator % i == 0){
                 numerator /= i;
                 tempDenominator /= i;
             }
         }
-        if (tempDenominator <= num && numerator != 0)
-        {
+        if (tempDenominator <= num && numerator != 0){
             std::cout << numerator << '/' << tempDenominator << '\t';
         }
     }
@@ -54,14 +47,12 @@ void fractPrint(int32_t num, int32_t denominator)
 
 int main() 
 {
-    try
-    {
+    try{
         int32_t num = inputNumber();
         int32_t denominator = factorial(num);
         fractPrint(num, denominator);
     }
-    catch (const char* e)
-    {
+    catch (const char* e){
         std::cout << e;
     }
     return 0;
