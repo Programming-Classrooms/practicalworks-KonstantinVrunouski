6f0f356fe1c9b7b381bc93b7e10fc1c9b3c1bcd4
KonstantinVrunouski <enotsith@gmail.com>
2023-12-24 21:30:57 +0300

    Исправления скобок.

--- laboratory-task-4/main.cpp
+++ laboratory-task-4/main.cpp
@@ -17,14 +17,16 @@ size_t inputSquareMtrxSize()
 	size_t size;
 	std::cout << "Input size of the matrix:\n";
 	std::cin >> size;
+	if (size <= 0){
+		throw "Invalid size.";
+	}
 	return size;
 }
 
 void allocateMemoryForMtrx(int32_t**& mtrx, size_t size)
 {
 	mtrx = new int32_t * [size];
-	for (size_t i = 0; i < size; ++i)
-	{
+	for (size_t i = 0; i < size; ++i){
 		mtrx[i] = new int32_t[size];
 	}
 }
@@ -34,8 +36,7 @@ int32_t inputType()
 	std::cout << "How'd you like to input elements:\n1)Manually.\t2)Randomly.\n";
 	int32_t inType;
 	std::cin >> inType;
-	if (inType != 1 && inType != 2)
-	{
+	if (inType != 1 && inType != 2){
 		throw "Invalid choice.";
 	}
 	return inType;
@@ -44,35 +45,35 @@ int32_t inputType()
 void mtrxFillManual(int32_t** mtrx, size_t size)
 {
 	std::cout << "Input elements of the matrix:\n";
-	for (size_t i = 0; i < size; ++i)
-		for (size_t j = 0; j < size; ++j)
+	for (size_t i = 0; i < size; ++i){
+		for (size_t j = 0; j < size; ++j){
 			std::cin >> mtrx[i][j];
+		}
+	}
 }
 
 void inputBorders(int32_t& lBord, int32_t& rBord)
 {
 	std::cout << "Input left and right border of random:\n";
 	std::cin >> lBord >> rBord;
-	if (lBord > rBord)
-		std::swap(lBord, rBord);
 }
 
 void mtrxFillRandomly(int32_t** mtrx, size_t size)
 {
 	int32_t lBord, rBord;
 	inputBorders(lBord, rBord);
-	for (size_t i = 0; i < size; ++i)
-		for (size_t j = 0; j < size; ++j)
+	for (size_t i = 0; i < size; ++i){
+		for (size_t j = 0; j < size; ++j){
 			mtrx[i][j] = lBord + rand() % (rBord - lBord + 1);
+		}
+	}
 }
 
 void printMtrx(int32_t** mtrx, size_t size, const char* message = "Generated matrix:")
 {
 	std::cout << message << '\n';
-	for (size_t i = 0; i < size; ++i)
-	{
-		for (size_t j = 0; j < size; ++j)
-		{
+	for (size_t i = 0; i < size; ++i){
+		for (size_t j = 0; j < size; ++j){
 			std::cout << std::setw(5) << mtrx[i][j] << '\t';
 		}
 		std::cout << '\n';
@@ -81,17 +82,16 @@ void printMtrx(int32_t** mtrx, size_t size, const char* message = "Generated mat
 
 void cleanMemory(int32_t**& mtrx, size_t size)
 {
-	for (size_t i = 0; i < size; ++i)
+	for (size_t i = 0; i < size; ++i){
 		delete[] mtrx[i];
+	}
 	delete[] mtrx;
 }
 
 bool isPositiveElementInColumn(int32_t** mtrx, size_t size, size_t col)
 {
-	for (size_t i = 0; i < size; ++i)
-	{
-		if (mtrx[i][col] > 0)
-		{
+	for (size_t i = 0; i < size; ++i){
+		if (mtrx[i][col] > 0){
 			return true;
 		}
 	}
@@ -101,14 +101,12 @@ bool isPositiveElementInColumn(int32_t** mtrx, size_t size, size_t col)
 int32_t maxElementInColumn(int32_t** mtrx, size_t size)
 {
 	int32_t max = INT32_MIN;
-	for (size_t i = 0; i < size; ++i)
-	{
-		if (!isPositiveElementInColumn(mtrx, size, i))
-		{
-			for (size_t j = 0; j < size; ++j)
-			{
-				if (mtrx[j][i] > max)
+	for (size_t i = 0; i < size; ++i){
+		if (!isPositiveElementInColumn(mtrx, size, i)){
+			for (size_t j = 0; j < size; ++j){
+				if (mtrx[j][i] > max){
 					max = mtrx[j][i];
+				}
 			}
 		}
 	}
@@ -118,12 +116,9 @@ int32_t maxElementInColumn(int32_t** mtrx, size_t size)
 size_t countNegativeElementsInLowRightTriangle(int32_t** mtrx, size_t size)
 {
 	size_t k = 0, cnt = 0;
-	for (size_t i = size - 1; i >= 0; --i)
-	{
-		for (size_t j = k; j < size; ++j)
-		{
-			if (mtrx[i][j] < 0)
-			{
+	for (size_t i = size - 1; i >= 0; --i){
+		for (size_t j = k; j < size; ++j){
+			if (mtrx[i][j] < 0){
 				++cnt;
 			}
 		}
@@ -134,44 +129,51 @@ size_t countNegativeElementsInLowRightTriangle(int32_t** mtrx, size_t size)
 
 void print(int32_t max, size_t cnt)
 {
-	if (max == INT32_MIN)
+	if (max == INT32_MIN){
 		std::cout << "There is no maximum element, that satisfies condition.\n";
-	else
+	}
+	else{
 		std::cout << "Maximum element is " << max << ".\n";
-	if (cnt != 0)
+	}
+	if (cnt != 0){
 		std::cout << "Count of negative elements in right down triangle: " << cnt << ".\n";
-	else
+	}
+	else{
 		std::cout << "There are no negative elements in right down triangle.\n";
+	}
 }
 
 int main()
 {
 	srand(time(NULL));
+	
 	int32_t** mtrx = nullptr;
 	size_t size;
-	try
-	{
+
+	try{
 		size = inputSquareMtrxSize();
 		allocateMemoryForMtrx(mtrx, size);
 
 		int32_t in_type = inputType();
-		if (in_type == 1)
+		if (in_type){
 			mtrxFillManual(mtrx, size);
-		else
+		}
+		else{
 			mtrxFillRandomly(mtrx, size);
+		}
 
 		int32_t max = maxElementInColumn(mtrx, size);
 		
 		size_t cnt = countNegativeElementsInLowRightTriangle(mtrx, size);
 		
-		if (in_type != 1)
+		if (!in_type){
 			printMtrx(mtrx, size);
-		
+		}
+
 		print(max,cnt);
 		cleanMemory(mtrx, size);
 	}
-	catch (const char* e)
-	{
+	catch (const char* e){
 		std::cout << e;
 		cleanMemory(mtrx, size);
 	}
